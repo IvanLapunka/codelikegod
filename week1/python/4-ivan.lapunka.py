bf86015696002a111bcfa5b83691861f402cb8f7
Ivan Lapunka <ivan.lapunka@gmail.com>
2019-04-24 14:56:47 +0300

    Долго думал над тем как бы это все унифицировать - получилось с помощью двух векторов

---
+++ week1/python/4-ivan.lapunka.py
@@ -0,0 +1,28 @@
+class Solution:
+    leng = 0
+    pattern1 = [1, 0, -1, 0]
+    pattern2 = [0, 1, 1, 0]
+
+    def setLeng(self, matrix):
+        self.leng = len(matrix[0]) - 1
+
+    def rotate(self, matrix):
+        self.setLeng(matrix)
+        for i in range(len(matrix[0]) // 2):
+            for j in range(i, len(matrix[0]) - i - 1):
+                self.changeCell(matrix, i, j)
+
+    def changeCell(self, matrix, i, j):
+        tmp1, tmp2 = 0, 0
+        tmp1 = matrix[self.genNextRow(0, i, j)][self.genNextCol(0, i, j)]
+        for ind in range(0, 5):
+            tmp2 = tmp1
+            nInd = (ind + 1) % 4
+            tmp1 = matrix[self.genNextRow(nInd, i, j)][self.genNextCol(nInd, i, j)]
+            matrix[self.genNextRow(nInd, i, j)][self.genNextCol(nInd, i, j)] = tmp2
+
+    def genNextRow(self, k, r, c):
+        return r * self.pattern1[k] + c * self.pattern1[(k + 3) % 4] + self.leng * self.pattern2[(k + 3) % 4]
+
+    def genNextCol(self, k, r, c):
+        return r * self.pattern1[(k + 1) % 4] + c * self.pattern1[k] + self.leng * self.pattern2[k]
