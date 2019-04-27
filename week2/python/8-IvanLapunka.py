f0efdc3ba2e73b4048136c15c0891724e5a618b2
Ioan <ivan.lapunka@gmail.com>
2019-04-28 01:54:57 +0300

    Разделяй и властвуй

--- week2/python/8-IvanLapunka.py
+++ week2/python/8-IvanLapunka.py
@@ -24,36 +24,3 @@ class Solution:
             self.RecSearch(matrix, target, middle_r + 1, middle_c + 1, right_down_r, right_down_c)
             self.RecSearch(matrix, target, middle_r + 1, left_up_c, right_down_r, middle_c)
             self.RecSearch(matrix, target, left_up_r, middle_c + 1, middle_r, right_down_c)
-
-
-test = [
-  [1,   4,  7, 11, 15],
-  [2,   5,  8, 12, 19],
-  [3,   6,  9, 16, 22],
-  [10, 13, 14, 17, 24],
-  [18, 21, 23, 26, 30]
-]
-
-test2 = [
-  [1,   4,  7, 11, 15]
-]
-
-test3 = [[1], [2]]
-
-test4 = []
-
-s = Solution()
-print(s.searchMatrix(test, 5))
-s1 = Solution()
-print(s1.searchMatrix(test, 20))
-
-s2 = Solution()
-print(s2.searchMatrix(test2, 15))
-s3 = Solution()
-print(s3.searchMatrix(test2, 5))
-
-
-s4 = Solution()
-print(s4.searchMatrix(test3, 15))
-# s5 = Solution()
-# print(s1.searchMatrix(test4, 5))
